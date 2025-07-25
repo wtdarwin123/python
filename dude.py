7ef5ea6a34099502688c760fec6855cc22553115
wtdarwin123 <131159722+wtdarwin123@users.noreply.github.com>
2025-07-25 23:14:14 +0300

    Create dude.py
    yeah its js a dud

---
+++ dude.py
@@ -0,0 +1,13 @@
+import time
+
+print("hi")
+time.sleep(1)
+ans=input('wanna see smthing cool? [y or n]  ')
+if ans == "y":
+    print("well i dont have anything rn maybe come later?")
+if ans == "n":
+    print("yeah well i didnt wanna show u anything anyway.")
+    
+else:
+        print("i said y or n!!!")
+  
